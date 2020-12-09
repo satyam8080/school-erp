a10334a8846964543ef5fefa814a91f7a8bfa76f
Anmol <anmolnagvanshi04@gmail.com>
2020-12-09 14:42:36 +0530

    added assignment table

--- models/assignment.py
+++ models/assignment.py
@@ -1,23 +1,20 @@
 from app import db
 from datetime import datetime, timedelta
 
+
 def get_due_date(self):
-      return datetime.utcnow() + timedelta(days=2)
+    return datetime.utcnow() + timedelta(days=2)
 
 
 class Assignment(db.Model):
     __tablename__ = 'assignments'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    assignment_file = db.Column(db.String(512), nullable=True)
-    comment = db.Column(db.String(512), nullable=True)
+    file = db.Column(db.String(512), nullable=True)
+    text = db.Column(db.String(5000), nullable=True)
     date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     due_date = db.Column(db.DateTime, default=get_due_date, nullable=False)
-
-    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), index=True, nullable=False)
-    # assignment_id = db.Column(db.Integer, index=True, nullable=False)     
 
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
-
